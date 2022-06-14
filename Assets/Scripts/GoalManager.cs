ca2c700fe71686868356915137f67f2ece05f5e7
cevdetozbey <38431368+cevozby@users.noreply.github.com>
2022-06-14 17:07:44 +0300

    Goal systems and effects added

---
+++ Assets/Scripts/GoalManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GoalManager : MonoBehaviour
+{
+    [SerializeField] ParticleSystem upConfetti;
+    [SerializeField] ParticleSystem leftConfetti;
+    [SerializeField] ParticleSystem rightConfetti;
+
+    [SerializeField] GameObject goalText;
+
+    [SerializeField] float time;
+
+    public static bool goalCheck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        goalCheck = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("SoccerGoal"))
+        {
+            Debug.Log("Goal");
+            goalCheck = true;
+            StartCoroutine(ConfettiTimer(time));
+        }
+    }
+
+    IEnumerator ConfettiTimer(float time)
+    {
+        upConfetti.Play();
+        leftConfetti.Play();
+        rightConfetti.Play();
+        goalText.SetActive(true);
+        yield return new WaitForSeconds(time);
+        upConfetti.Stop();
+        leftConfetti.Stop();
+        rightConfetti.Stop();
+        goalText.SetActive(false);
+    }
+
+}
--- Assets/Scripts/Shoting.cs
+++ Assets/Scripts/Shoting.cs
@@ -11,32 +11,65 @@ public class Shoting : MonoBehaviour
 
     public Vector3 lookForce;
 
-    //[SerializeField] GameObject realBall;
     Rigidbody ballRB;
+
+    public static bool shotCheck;
+
+    Vector3 worldPos;
+    [SerializeField] float angleSpeed;
+    float angle;
 
 
     // Start is called before the first frame update
     void Start()
     {
         ballRB = GetComponent<Rigidbody>();
+        shotCheck = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         ShotControl();
-        drawLine.SimulateDottedLine(ball, transform.position, transform.forward*force);
-        //lookForce = transform.forward;
+        
+        if (Input.GetMouseButton(0))
+        {
+            if (!shotCheck)
+            {
+                drawLine.SimulateDottedLine(ball, transform.position, transform.forward * force);
+            }
+            BallAngelControl();
+        }
+
+        if (GoalManager.goalCheck)
+        {
+            Debug.Log("Gol oldu");
+            ballRB.velocity = Vector3.zero;
+        }
+        
+    }
+
+    void BallAngelControl()
+    {
+        
+        //Debug.Log(Input.GetAxis("Mouse X"));
+        angle += Input.GetAxis("Mouse X") * angleSpeed * Time.deltaTime;
+        //transform.Rotate(Vector3.up * angle);
+        angle = Mathf.Clamp(angle, 30, 150);
+        //transform.localRotation = Quaternion.AngleAxis(angle, Vector3.up);
+        transform.localRotation = Quaternion.Euler(angle * Vector3.up);
+
     }
 
     void ShotControl()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonUp(0))
         {
             //var spawned = Instantiate(ball, transform.position, transform.rotation);
 
             //spawned.Init(transform.forward * force);
             ballRB.velocity = transform.forward * force;
+            shotCheck = true;
         }
     }
 }
